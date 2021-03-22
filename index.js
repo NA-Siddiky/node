c570cf022f3e3882427b03ec725e57f391d1e7e2
NA-Siddiky <siddiky.academic@gmail.com>
2021-03-23 01:15:58 +0600

    adding nodemon

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const app = express();
 // const rootCall = (request, response) => response.send("Thankyou")
 
 app.get('/', (request, response) => {
-    response.send("Thanks A LOT")
+    const fruit = {
+        product: 'ada',
+        price: 220,
+    }
+    response.send(fruit);
 })
 
-app.listen(4000, () => console.log('Listening to port 3000'))
+app.listen(3000, () => console.log('Listening to port 3000'))
