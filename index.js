458b2b15827533747fe9e0d1b70aca1485bfc32e
NA-Siddiky <siddiky.academic@gmail.com>
2021-03-24 04:02:31 +0600

    adding post data

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
 const users = ["Asad", "Moin", "Sabid", "Susmita", "Sohana", "Sabana"]
-// const rootCall = (request, response) => response.send("Thankyou")
 
+// get
 app.get('/', (request, response) => {
     const fruit = {
         product: 'ada',
@@ -12,17 +17,20 @@ app.get('/', (request, response) => {
     }
     response.send(fruit);
 });
-app.get('/fruits/banana', (request, response) => {
-    response.send({ fruit: 'banana', quantity: 100, price: 100 })
-})
 
 app.get('/users/:id', (request, response) => {
     // console.log(request.params.id);
     const id = request.params.id;
-    console.log(request.query.sort);
     const name = users[id];
     response.send({ id, name });
 })
 
+// post 
+app.post('/addUser', (req, res) => {
+    // console.log("data-received", req.body);
+    const user = req.body;
+    user.id = 55;
+    res.send(user)
+})
 const port = 4000;
 app.listen(port, () => console.log('Listening to port ' + port))
